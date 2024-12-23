4fced5967c9f5a66e4b6b81f4e6049eb310eaf04
mipjobs123 <108760737+mipjobs123@users.noreply.github.com>
2024-12-23 23:06:52 +0300

    Updated navigation and content display logic

--- script.js
+++ script.js
@@ -3,11 +3,18 @@ const listItems = document.querySelectorAll('nav ul li')
 
 listItems.forEach((item, idx) => {
     item.addEventListener('click', () => {
+        const targetIndex = item.getAttribute('data-target');  
         hideAllContents()
         hideAllItems()
 
         item.classList.add('active')
-        contents[idx].classList.add('show')
+        contents.forEach(content => {
+            if (content.getAttribute('data-index') == targetIndex) {
+                content.classList.add('show');
+            }  
+        });
+        // contents[targetIndex].classList.add('show')
+
     })
 })
 
